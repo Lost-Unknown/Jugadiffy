024dfaf713f692b66bee067dd889eada08e746fe
Lost Unknown <unknownflicks@gmail.com>
2023-08-03 19:26:35 +0530

    search half-2

---
+++ app/FAQ/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FAQPage = () => {
+  return (
+    <section className='flex flex-col gap-4 items-start'>
+        <h1 className='text-3xl text-white'>Frequently Asked Question</h1>
+        <p className='text-xl text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eius, corrupti, ducimus, exercitationem tenetur debitis quam aliquam quis dolore dolores totam. Earum praesentium nulla, reiciendis hic sed iste libero esse.</p>
+    </section>
+  )
+}
+
+export default FAQPage
---
+++ app/TermCondition/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TermConditionPage = () => {
+  return (
+    <section className='flex flex-col gap-4 items-start'>
+        <h1 className='text-3xl text-white'>Term & Condition</h1>
+        <p className='text-xl text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eius, corrupti, ducimus, exercitationem tenetur debitis quam aliquam quis dolore dolores totam. Earum praesentium nulla, reiciendis hic sed iste libero esse.</p>
+    </section>
+  )
+}
+
+export default TermConditionPage
